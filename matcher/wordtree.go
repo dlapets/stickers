7b02ac4903720fba505f1569ddef081dcc111b2e
dlapets <lapets@adjust.com>
2019-09-05 16:14:11 +0200

    Keep track of remainders in word tree

--- matcher/wordtree.go
+++ matcher/wordtree.go
@@ -54,6 +54,69 @@ func (t *WordTree) Find(word string) []string {
 	return curTree.words
 }
 
+type Result struct {
+	words     []string
+	remainder string
+}
+
+func (t *WordTree) FindAll(word string) [][]string {
+	wordsOnly := [][]string{}
+	for _, result := range findAll(0, t, []rune{}, []rune(wordHash(word))) {
+		wordsOnly = append(wordsOnly, result.words)
+	}
+	return wordsOnly
+}
+
+func findAll(level int, cur *WordTree, remainderRunes []rune, givenRunes []rune) []Result {
+	lprintf(level, "findAll: called with level: %d, remainderRunes: %s, givenRunes: %s\n", level, string(remainderRunes), string(givenRunes))
+
+	found := []Result{}
+
+	// There are words at this node, that means we can take them.
+	if len(cur.words) > 0 {
+		newRemainderRunes := []rune{}
+		newRemainderRunes = append(newRemainderRunes, remainderRunes...)
+		if len(givenRunes) > 0 {
+			newRemainderRunes = append(newRemainderRunes, givenRunes...)
+		}
+
+		result := Result{words: cur.words, remainder: normalizeRemainderRunes(newRemainderRunes)}
+		lprintf(level, "findAll: adding result: %s\n", result)
+		found = append(found, result)
+	}
+
+	var prev rune
+	for i, r := range givenRunes {
+		if r == prev {
+			lprintf(level, "findAll: rune: %s: skipping duplicate\n", string(r))
+			continue
+		}
+
+		// TODO see if we can avoid copy here...
+		newRemainderRunes := []rune{}
+		newRemainderRunes = append(newRemainderRunes, remainderRunes...)
+		newRemainderRunes = append(newRemainderRunes, givenRunes[0:i]...)
+
+		if next, ok := cur.children[r]; ok {
+			// Add all words found in subtrees
+			nextRunes := givenRunes[i+1:]
+			lprintf(level, "findAll: rune: %s: recursing with remainderRunes: %s, nextRunes: %s\n", string(r), string(newRemainderRunes), string(nextRunes))
+			newFound := findAll(level+1, next, newRemainderRunes, nextRunes)
+			found = append(found, newFound...)
+		} else {
+			lprintf(level, "findAll: rune: %s: no children\n", string(r))
+		}
+		prev = r
+	}
+
+	lprintf(level, "findAll: return %s\n", found)
+	return found
+}
+
+func normalizeRemainderRunes(runes []rune) string {
+	return wordHash(string(runes))
+}
+
 func (t *WordTree) String() string {
 	return t.draw("root", 0)
 }
@@ -64,43 +127,6 @@ func (t *WordTree) draw(label string, level int) string {
 		s += child.draw(string(r), level+1)
 	}
 	return s
-}
-
-func (t *WordTree) FindAll(word string) [][]string {
-	return findAll(0, t, []rune(wordHash(word)))
-}
-
-func findAll(level int, cur *WordTree, givenRunes []rune) [][]string {
-	lprintf(level, "findAll: called with level: %d, givenRunes: %s\n", level, string(givenRunes))
-
-	found := [][]string{}
-
-	// There are words at this node, that means we can take them.
-	if len(cur.words) > 0 {
-		lprintf(level, "findAll: adding words: %s\n", cur.words)
-		found = append(found, cur.words)
-	}
-
-	var prev rune
-	for i, r := range givenRunes {
-		if r == prev {
-			lprintf(level, "findAll: rune: %s: skipping duplicate\n", string(r))
-			continue
-		}
-		if next, ok := cur.children[r]; ok {
-			// Add all words found in subtrees
-			nextRunes := givenRunes[i+1:]
-			lprintf(level, "findAll: rune: %s: recursing with nextRunes: %s\n", string(r), string(nextRunes))
-			newFound := findAll(level+1, next, nextRunes)
-			found = append(found, newFound...)
-		} else {
-			lprintf(level, "findAll: rune: %s: no children\n", string(r))
-		}
-		prev = r
-	}
-
-	lprintf(level, "findAll: return %s\n", found)
-	return found
 }
 
 func lsprintf(level int, str string, stuff ...interface{}) string {
