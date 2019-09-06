c2f9bdfde57fbc9fde700cd13c110f47e9a01225
dlapets <lapets@adjust.com>
2019-09-06 19:47:46 +0200

    Clean up in word tree

--- internal/word_tree.go
+++ internal/word_tree.go
@@ -1,5 +1,7 @@
 package internal
 
+import "log"
+
 // WordTree is a structure for efficiently (?) identifying what words can be
 // formed from the runes of a given string.
 type WordTree struct {
@@ -17,19 +19,18 @@ func NewWordTree() *WordTree {
 // String returns a string representation of the tree. Note that this may span
 // multiple lines.
 func (t *WordTree) String() string {
-	return t.draw("root", 0)
+	return t.draw(0, "root")
 }
 
-func (t *WordTree) draw(label string, level int) string {
+func (t *WordTree) draw(level int, label string) string {
 	s := lsprintf(level, "%s words: %s\n", label, t.words)
 	for r, child := range t.children {
-		s += child.draw(string(r), level+1)
+		s += child.draw(level+1, string(r))
 	}
 	return s
 }
 
 // Add inserts word into the tree.
-// TODO handle duplicates!!
 func (t *WordTree) Add(word string) {
 	if len(word) == 0 {
 		return
@@ -44,8 +45,23 @@ func (t *WordTree) Add(word string) {
 		curTree = curTree.children[r]
 	}
 
+	// Check that the word has not already been added.
+	if sliceContainsWord(curTree.words, word) {
+		log.Printf("attempt to add duplicate word: %s", word)
+		return
+	}
+
 	// Add the word to the last subnode.
 	curTree.words = append(curTree.words, word)
+}
+
+func sliceContainsWord(words []string, word string) bool {
+	for _, oldWord := range words {
+		if oldWord == word {
+			return true
+		}
+	}
+	return false
 }
 
 // Find returns true if word is in the tree
@@ -58,12 +74,7 @@ func (t *WordTree) Find(givenWord string) bool {
 		}
 		curTree = curTree.children[r]
 	}
-	for _, word := range curTree.words {
-		if word == givenWord {
-			return true
-		}
-	}
-	return false
+	return sliceContainsWord(curTree.words, givenWord)
 }
 
 // Words finds all single words in the tree which can be made from the letters
@@ -77,37 +88,28 @@ func (t *WordTree) Words(givenWord string) []string {
 }
 
 func findAll(level int, cur *WordTree, givenRunes []rune) []*result {
-	//lprintf(level, "findAll: called with level: %d, givenRunes: %s\n", level, string(givenRunes))
-
+	// found contains all matching words we found so far.
 	found := []*result{}
 
-	// There are words at this node, that means we can take them.
+	// There are words at this node, we can use these words.
 	if len(cur.words) > 0 {
 		result := newResult(cur.words)
-		//lprintf(level, "findAll: adding result: %s\n", result)
 		found = append(found, result)
 	}
 
 	var prev rune
 	for i, r := range givenRunes {
 		if r == prev {
-			//lprintf(level, "findAll: rune: %s: skipping duplicate\n", string(r))
 			continue
 		}
-
 		if next, ok := cur.children[r]; ok {
 			// Add all words found in subtrees
 			nextRunes := givenRunes[i+1:]
-			//lprintf(level, "findAll: rune: %s: recursing with nextRunes: %s\n", string(r), string(nextRunes))
 			newFound := findAll(level+1, next, nextRunes)
 			found = append(found, newFound...)
-		} else {
-			//lprintf(level, "findAll: rune: %s: no children\n", string(r))
 		}
 		prev = r
 	}
-
-	//lprintf(level, "findAll: return %s\n", found)
 	return found
 }
 
